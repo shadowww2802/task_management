4bb16e0ef122af439388459f7888724ceb0df769
roaJeffrey <125330636+roaJeffrey@users.noreply.github.com>
2024-02-26 15:45:35 +0800

    test2

--- HomeScreen.php
+++ HomeScreen.php
@@ -29,7 +29,7 @@
                     <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
 
                     <div class="form-outline mb-4">
-                      <label class="form-label" for="form2Example17">Email aaddress</label>
+                      <label class="form-label" for="form2Example17">Email address</label>
                       <input type="email" id="form2Example17" class="form-control form-control-lg" />
                     </div>
 
