613c3df177a9591f1d9b0b6b2d09da7295e0ee5d
shadowww2802 <cumpio.justinejay28@gmail.com>
2024-02-27 12:46:26 +0800

    random backend testing

--- LogInUI.php
+++ LogInUI.php
@@ -6,8 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -18,7 +17,7 @@
           <div class="card" style="border-radius: 1rem;">
             <div class="row g-0">
 
-            <!-- Side image -->
+              <!-- Side image -->
               <div class="col-md-6 col-lg-5 d-none d-md-block align-self-center">
                 <img src="../task_management/img/Login.jpg" alt="Login Image" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
               </div>
@@ -26,7 +25,7 @@
               <div class="col-md-6 col-lg-7 d-flex align-items-center">
                 <div class="card-body p-4 p-lg-5 text-black">
 
-                  <form>
+                  <form action="process.php" method="post">
                     <div class="d-flex align-items-center mb-3 pb-1 mt-3">
                       <span class="h1 fw-bold mb-0">Welcome!</span>
                     </div>
@@ -34,20 +33,21 @@
 
                     <div class="form-outline mb-4">
                       <label class="form-label" for="form2Example17">Email address</label>
-                      <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                      <input name="email" type="email" id="form2Example17" class="form-control form-control-lg"/>
                     </div>
                     <div class="form-outline mb-4">
                       <label class="form-label" for="form2Example27">Password</label>
-                      <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                      <input name="user_password" type="password" id="form2Example27" class="form-control form-control-lg"/>
                     </div>
 
                     <div class="pt-1 mb-4">
-                      <button class="btn btn-primary btn-lg btn-block w-100" type="button">Login</button>
+                      <button class="btn btn-primary btn-lg btn-block w-100" type="submit" name="loginButton">Login</button>
                     </div>
 
                     <!-- Navigation to RegistrationUI -->
-                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? 
-                    <a href="RegistrationUI.php" style="color: #393f81;">Register here</a></p>
+                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account?
+                      <a href="RegistrationUI.php" style="color: #393f81;">Register here</a>
+                    </p>
                   </form>
                 </div>
               </div>
@@ -57,8 +57,7 @@
       </div>
     </div>
   </section>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
   <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <?php
