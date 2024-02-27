d1323aa6fc09fb599a615fe5145c9dad5999aaa6
shadowww2802 <cumpio.justinejay28@gmail.com>
2024-02-27 11:00:30 +0800

    dbconfig and process test

--- LogInUI.php
+++ LogInUI.php
@@ -1,11 +1,14 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task Manager</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Task Manager</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
   <section class="vh-100" style="background-color: #4b4df7;">
     <div class="container py-5 h-100">
@@ -19,7 +22,7 @@
               <div class="col-md-6 col-lg-7 d-flex align-items-center">
                 <div class="card-body p-4 p-lg-5 text-black">
 
-                  <form>
+                  <form action="process.php" method="POST" autocomplete="off">
 
                     <div class="d-flex align-items-center mb-3 pb-1 mt-3">
                       <span class="h1 fw-bold mb-0">Welcome User!</span>
@@ -29,19 +32,20 @@
 
                     <div class="form-outline mb-4">
                       <label class="form-label" for="form2Example17">Email address</label>
-                      <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                      <input type="email" id="form2Example17" class="form-control form-control-lg" name="email" required />
                     </div>
 
                     <div class="form-outline mb-4">
                       <label class="form-label" for="form2Example27">Password</label>
-                      <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                      <input type="password" id="form2Example27" class="form-control form-control-lg" name="user_password" required />
                     </div>
 
                     <div class="pt-1 mb-4">
-                      <button class="btn btn-primary btn-lg btn-block w-100" type="button">Login</button>
+                      <button type="submit" class="btn btn-primary btn-lg btn-block w-100" name="loginButton">Login</button>
                     </div>
-                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? 
-                    <a href="#!" style="color: #393f81;">Register here</a></p>
+                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account?
+                      <a href="#!" style="color: #393f81;">Register here</a>
+                    </p>
                   </form>
 
                 </div>
@@ -53,5 +57,23 @@
     </div>
   </section>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+  <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <?php
+  if (isset($_SESSION['status']) && $_SESSION['status_code'] != '') {
+  ?>
+    <script>
+      swal({
+        title: "<?php echo $_SESSION['status']; ?>",
+        icon: "<?php echo $_SESSION['status_code']; ?>",
+      });
+    </script>
+  <?php
+    unset($_SESSION['status']);
+    unset($_SESSION['status_code']);
+  }
+  ?> -->
+
 </body>
+
 </html>
